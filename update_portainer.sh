05e13e2b93eca2af7ee76b875ee4bba872e96425
Xavier Wong <xavier.dht@gmail.com>
2021-12-08 22:26:07 +0800

    Create update_portainer.sh

---
+++ update_portainer.sh
@@ -0,0 +1,7 @@
+portainer_pid=`docker ps | grep portainer-ce | awk '{print $1}'`
+portainer_name=`docker ps | grep portainer-ce | awk '{print $2}'`
+
+sudo docker stop $portainer_pid
+sudo docker rm $portainer_pid
+sudo docker rmi $portainer_name
+sudo docker run -d -p 9000:9000 -p 9443:9443 --name=portainer --restart=always -v /var/run/docker.sock:/var/run/docker.sock -v portainer_data:/data portainer/portainer-ce:latest
